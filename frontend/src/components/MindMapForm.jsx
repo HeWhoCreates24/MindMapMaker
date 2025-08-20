ab7aaed6b3d63fd6e78e6cbd19cf6ad255d5fa2c
Tanmay Hatkar <tanmayhatkar240@gmail.com>
2025-08-20 10:24:47 +0530

    ux enhancement

--- frontend/src/components/MindMapForm.jsx
+++ frontend/src/components/MindMapForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function MindMapForm() {
   const [title, setTitle] = useState("");
@@ -8,7 +8,7 @@ export default function MindMapForm() {
   const [svgUrl, setSvgUrl] = useState("");
 
   const addNode = () => setNodes([...nodes, { id: "", label: "", type: "text" }]);
-  const addEdge = () => setEdges([...edges, { from_node: "", to_node: "", label: "", dashed: false }]);
+  const addEdge = () => setEdges([...edges, { from_node: "", to_node: "", label: "", dashed: false, id: crypto.randomUUID() }]);
 
   const handleNodeChange = (i, field, value) => {
     const newNodes = [...nodes];
@@ -22,82 +22,91 @@ export default function MindMapForm() {
     setEdges(newEdges);
   };
 
-  const isValidMap = () => {
-    if(!title || !nodes.length) return false
-    else return true
-  }
-
   const generateMap = async () => {
-    if(isValidMap()){
-      await fetch("https://mindmapmaker-ld69.onrender.com/generate", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ title, layout, nodes, edges })
-      });
-      setSvgUrl(`https://mindmapmaker-ld69.onrender.com/map?${Date.now()}`); // prevent caching
-    }else{
-      alert("Please input a valid Map")
-    }
+    await fetch("https://mindmapmaker-ld69.onrender.com/generate", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title, layout, nodes, edges })
+    });
+    setSvgUrl(`https://mindmapmaker-ld69.onrender.com/map?${Date.now()}`); // prevent caching
   };
 
+  useEffect(() => {
+    generateMap()
+  }, [title, layout, nodes, edges])
+
   return (
-    <div id="form-container">
+    <div id="main-container">
       <h1>MindMap Maker</h1>
-      <input value={title} onChange={e => setTitle(e.target.value)} placeholder="Map Title" />
+      <div id="form-container">
 
-      <h2>Nodes</h2>
-      {nodes.map((node, i) => (
-        <div className="input-container" key={i}>
-          <input placeholder="ID" value={node.id} onChange={e => handleNodeChange(i, "id", e.target.value)} />
-          <input placeholder="Label" value={node.label} onChange={e => handleNodeChange(i, "label", e.target.value)} />
-          <select value={node.type} onChange={e => handleNodeChange(i, "type", e.target.value)}>
-            <option value="h1">H1</option>
-            <option value="h2">H2</option>
-            <option value="h3">H3</option>
-            <option value="text">Text</option>
-            <option value="formula">Formula</option>
-          </select>
+          <h2>MindMap Data</h2>
+        <div className="input-group-container">
+          <div className="input-group">
+            <h3>Map Title</h3>
+            <input value={title} onChange={e => setTitle(e.target.value)} placeholder="Map Title" />
+          </div>
+
+          <div className="input-group">
+            <h3>Map Layout</h3>
+            <select value={layout} onChange={e => setLayout(e.target.value)}>
+              <option value="dot">Dot</option>
+              <option value="neato">Neato</option>
+              <option value="fdp">Fdp</option>
+              <option value="sfdp">Sfdp</option>
+              <option value="circo">Circo</option>
+              <option value="twopi">Twopi</option>
+              <option value="patchwork">Patch-Work</option>
+              <option value="osage">Osage</option>
+            </select>
+          </div>
         </div>
-      ))}
-      <button onClick={addNode}>Add Node</button>
 
-      <h2>Edges</h2>
-      {edges.map((edge, i) => (
-        <div className="input-container" key={i}>
-          <input placeholder="From" value={edge.from_node} onChange={e => handleEdgeChange(i, "from_node", e.target.value)} />
-          <input placeholder="To" value={edge.to_node} onChange={e => handleEdgeChange(i, "to_node", e.target.value)} />
-          <input placeholder="Label" value={edge.label} onChange={e => handleEdgeChange(i, "label", e.target.value)} />
-          <label>
-            Dashed
-            <input type="checkbox" checked={edge.dashed} onChange={e => handleEdgeChange(i, "dashed", e.target.checked)} />
-          </label>
+
+        <div className="input-group-container">
+          <div className="input-group">
+            <h3>Nodes</h3>
+            {nodes.map((node, i) => (
+              <div className="input-container" key={i}>
+                <input placeholder="ID" value={node.id} onChange={e => handleNodeChange(i, "id", e.target.value)} />
+                <input placeholder="Label" value={node.label} onChange={e => handleNodeChange(i, "label", e.target.value)} />
+                <select value={node.type} onChange={e => handleNodeChange(i, "type", e.target.value)}>
+                  <option value="h1">H1</option>
+                  <option value="h2">H2</option>
+                  <option value="h3">H3</option>
+                  <option value="text">Text</option>
+                  <option value="formula">Formula</option>
+                </select>
+                <button className="del" onClick={e => setNodes(prev => prev.filter(n => n.id != node.id))} >delete</button>
+              </div>
+            ))}
+            <button onClick={addNode}>Add Node</button>
+          </div>
+    
+          <div className="input-group">
+            <h3>Edges</h3>
+            {edges.map((edge, i) => (
+              <div className="input-container" key={i}>
+                <input placeholder="From" value={edge.from_node} onChange={e => handleEdgeChange(i, "from_node", e.target.value)} />
+                <input placeholder="To" value={edge.to_node} onChange={e => handleEdgeChange(i, "to_node", e.target.value)} />
+                <input placeholder="Label" value={edge.label} onChange={e => handleEdgeChange(i, "label", e.target.value)} />
+                <label>
+                  Dashed
+                  <input type="checkbox" checked={edge.dashed} onChange={e => handleEdgeChange(i, "dashed", e.target.checked)} />
+                </label>
+                <button className="del" onClick={e => setEdges(prev => prev.filter(e => e.id != edge.id))} >delete</button>
+              </div>
+            ))}
+            <button onClick={addEdge}>Add Edge</button>
+          </div>
         </div>
-      ))}
-      <button onClick={addEdge}>Add Edge</button>
 
-      <h2>Map Layout</h2>
-      <select value={layout} onChange={e => setLayout(e.target.value)}>
-        <option value="dot">Dot</option>
-        <option value="neato">Neato</option>
-        <option value="fdp">Fdp</option>
-        <option value="sfdp">Sfdp</option>
-        <option value="circo">Circo</option>
-        <option value="twopi">Twopi</option>
-        <option value="patchwork">Patch-Work</option>
-        <option value="osage">Osage</option>
-      </select>
-
-      <div>
-        <button onClick={generateMap}>Generate Map</button>
       </div>
 
-      {svgUrl && (
-        <div>
-          <h2>MindMap Preview</h2>
-          <br />
-          <img src={svgUrl} alt="MindMap" />
-        </div>
-      )}
+      <div id="preview-container" >
+        <h2>MindMap Preview</h2>
+        <img src={svgUrl} alt="MindMap" />
+      </div>
     </div>
   );
 }
